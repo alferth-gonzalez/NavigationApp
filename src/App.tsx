fda7b2d48e573172e355817ed5208621494d40d1
Alferth Gonzalez <alferthgonzalez@Alferths-MacBook-Air.local>
2024-11-04 13:29:48 -0500

    Estructura stackNavigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Text } from 'react-native';
+import { StackNavigator } from './presentation/routes/StackNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Text></Text>
+      <StackNavigator/>
     </NavigationContainer>
   );
 }
