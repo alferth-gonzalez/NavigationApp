675b3e6fe1fe24cc8548ba4e264c0db926fc03d4
Alferth Gonzalez <alferthgonzalez@Alferths-MacBook-Air.local>
2024-11-04 22:15:08 -0500

    Implementacion de PopToTop

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { globalStyles } from '../../theme/theme'
+import { PrimaryButton } from '../../components/shared/PrimaryButton'
+import { NavigationProp, StackActions, useNavigation } from '@react-navigation/native'
+import { RootStackParams } from '../../routes/StackNavigator'
 
 export const SettingsScreen = () => {
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
   return (
-    <View>
-      <Text>SettingsScreens</Text>
+    <View style={ globalStyles.container }>
+      <Text style={{ marginBottom: 10 }}>SettingsScreens</Text>
+
+      <PrimaryButton 
+        label='Regresar'
+        onPress={ () => navigation.goBack() }
+      />
+
+      <PrimaryButton 
+        label='Regresar al Home'
+        onPress={ () => navigation.dispatch( StackActions.popToTop() ) }
+      />
     </View>
   )
 }
