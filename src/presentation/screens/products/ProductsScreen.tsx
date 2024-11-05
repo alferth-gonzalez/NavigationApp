8908990b0cdb6bd25043c76e00de9131c19bc6c1
Alferth Gonzalez <alferthgonzalez@Alferths-MacBook-Air.local>
2024-11-04 20:56:11 -0500

    Implementacion de FlatList y navegacion de productos

--- src/presentation/screens/products/ProductsScreen.tsx
+++ src/presentation/screens/products/ProductsScreen.tsx
@@ -1,10 +1,51 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+import { FlatList, Text, View } from 'react-native'
+import { globalStyles } from '../../theme/theme';
+import { PrimaryButton } from '../../components/shared/PrimaryButton';
+
+const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 100,
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 200,
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: 300,
+  }
+];
 
 export const ProductsScreen = () => {
+
+  const navigation = useNavigation();
+
   return (
-    <View>
-        <Text>ProductsScreen</Text>
+    <View style={ globalStyles.container }>
+      <Text style={{ marginBottom: 10, fontSize: 30 }}>Productos</Text>
+
+      <FlatList 
+        data={products}
+        renderItem={ ({item}) => (
+          <PrimaryButton
+            onPress={ () => { navigation.navigate('Product' as never)} }
+            label={ item.name }
+          />
+        )}
+      />
+
+      <Text style={{ marginBottom: 10, fontSize: 30 }}>Ajustes</Text>
+      <PrimaryButton
+        onPress={ () => { navigation.navigate('Settings' as never) } }
+        label="Ir a ajustes"
+      />
     </View>
+
   )
 }
