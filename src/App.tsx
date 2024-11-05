f1f20225d927b06cbc58efcbdbd7e7b3644bf84e
Alferth Gonzalez <alferthgonzalez@Alferths-MacBook-Air.local>
2024-11-04 22:40:15 -0500

    Implementacion de Drawer navigation - sideMenu

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Text } from 'react-native';
 import { StackNavigator } from './presentation/routes/StackNavigator';
+import { SideMenuNavigator } from './presentation/routes/SideMenuNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator/>
+      {/* <StackNavigator/> */}
+      <SideMenuNavigator />
     </NavigationContainer>
   );
 }
