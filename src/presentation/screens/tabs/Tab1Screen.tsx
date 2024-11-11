a0dd33aad34c7e263da938e9b1655aa7006ebcd2
Alferth Gonzalez <alferthgonzalez@Alferths-MacBook-Air.local>
2024-11-11 15:06:09 -0500

    Instalacion de Ionicons para android y ios

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -3,12 +3,15 @@ import React, { useEffect } from 'react'
 import { Pressable, Text, View } from 'react-native'
 import { HamburgerMenu } from '../../components/shared/HamburgerMenu';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 export const Tab1Screen = () => {
 
   return (
     <View>
       <HamburgerMenu />
         <Text>Tab1Screen</Text> 
+        <Icon name="rocket-outline" size={100} />
     </View>
   )
 }
